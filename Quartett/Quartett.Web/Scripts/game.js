16b80ac80bcaf274330b488a87961f5b06e335ec
William Cowell <me@williamcowell.com>
2016-10-08 17:37:43 +0100

    Added game.js

---
+++ Quartett/Quartett.Web/Scripts/game.js
@@ -0,0 +1,12 @@
+﻿(function() {
+    var game = $.connection.gameHub;
+
+    // Client methods called by the server (see IPlayer for interface)
+    game.client.start = function (otherPlayer) {
+        
+    }
+
+    $.connection.hub.start().done(function() {
+
+    });
+})();
