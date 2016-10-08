2bdc3d71af4da43c05414b84b2af53dc58d60932
William Cowell <me@williamcowell.com>
2016-10-08 21:35:06 +0100

    Implemented moving played card to the end of the deck.

--- Quartett/Quartett.WebApi/Services/GameService.cs
+++ Quartett/Quartett.WebApi/Services/GameService.cs
@@ -53,7 +53,7 @@ namespace Quartett.WebApi.Services
                 ? game.Player2
                 : game.Player1;
 
-            Transfer(card: loserOfRound.NextCard, to: winnerOfRound.PlayerId);
+            await UpdateCards(winnerOfRound, loserOfRound).ConfigureAwait(false);
 
             return winnerOfRound.PlayerId;
         }
@@ -117,9 +117,40 @@ namespace Quartett.WebApi.Services
             return player.NextCard.Characteristics.Single(c => c.Name == characteristic);
         }
 
-        private void Transfer(Card card, string to)
+        private async Task UpdateCards(Player winnerOfRound, Player loserOfRound)
         {
-            throw new NotImplementedException();
+            var game = await _repository.GetGame().ConfigureAwait(false);
+
+            TransferCardToWinner(
+                ref game,
+                losingCard: loserOfRound.NextCard,
+                winnerId: winnerOfRound.PlayerId);
+
+            MoveCurrentCardToEndOfDeck(ref game, game.Player1Id);
+            MoveCurrentCardToEndOfDeck(ref game, game.Player2Id);
+
+            await _repository.UpdateGame(game).ConfigureAwait(false);
         }
+
+        private static void TransferCardToWinner(ref Contexts.Entities.Game game, Card losingCard, string winnerId)
+        {
+            var allCards = game.PlayerCards.ToArray();
+            var cardToTransfer = allCards.Single(playerCard => playerCard.CardId == losingCard.Id);
+            var winnersCards = allCards.Where(playerCard => playerCard.PlayerId == winnerId).ToArray();
+            var lastWinnersCardOrder = winnersCards.Max(winnerCard => winnerCard.Order);
+
+            cardToTransfer.Order = lastWinnersCardOrder + 1;
+            cardToTransfer.PlayerId = winnerId;
+        }
+
+        private static void MoveCurrentCardToEndOfDeck(ref Contexts.Entities.Game game, string playerId)
+        {
+            var allCards = game.PlayerCards.ToArray();
+            var cards = allCards.Where(playerCard => playerCard.PlayerId == playerId).ToArray();
+            var lastCardOrder = cards.Max(card => card.Order);
+
+            cards.First().Order = lastCardOrder + 1;
+        }
+
     }
 }
