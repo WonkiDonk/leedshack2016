46b18a7c914800fc62e44a6bedfcb088da4646f1
William Cowell <me@williamcowell.com>
2016-10-09 10:08:03 +0100

    Formatted value

--- Quartett/Quartett.Web/Scripts/game.js
+++ Quartett/Quartett.Web/Scripts/game.js
@@ -63,7 +63,8 @@
                 $li.append($name);
 
                 var $value = $('<span class="value"></span>');
-                $value.text(character.Value);
+                var value = parseFloat(Math.round(character.Value * 100) / 100).toFixed(2);
+                $value.text(value);
                 $li.append($value);
             });
     };
